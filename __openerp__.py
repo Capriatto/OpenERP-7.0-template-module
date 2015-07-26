38a1cafc197ebf55b9ed28906166cbf6f00bf342
Jose_sebastian <michu-1@hotmail.com>
2015-07-25 22:57:34 -0500

    __openerp__.py template

--- __openerp__.py
+++ __openerp__.py
@@ -2,22 +2,17 @@
 # -*- coding: utf-8 -*-
 
 {
-	'name' : 'SAC: OPA',
+	'name' : '',
 	'version' : '1.0',
-	'depends' : ['base','crm', 'sac_configuracion', 'sac_reportes'],
-	'author' : 'zeta',
-	'category' : 'CRM',
-	'description' : """Modulo de configuracion""",
-	'website' : 'http://zeta.net.co/',
+	'depends' : ['base'],
+	'author' : '',
+	'category' : '',
+	'description' : '',
+	'website' : '',
 	'data' : [
-		'views/view_crm_case.xml',
-		'views/view_crm_case_section.xml',
-		'views/sac_crm_lead_productos_view.xml',
-		'views/sac_crm_oportunity_view.xml',
-		'views/sac_crm_lead_phone_view.xml',
-		'data/smtp.sql',
+
 	],
-	'qweb': ['static/src/xml/custom_access_login.xml'],
-    'css': ['static/src/css/custom_login.css','static/src/css/custom_field.css'],
+	'qweb': [''],
+    'css': [''],
 	'installable' : True
 }
